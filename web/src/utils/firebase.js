50b594b79bb1f94d00010d86423874ca5d92b53c
Pol Trujillo <103571473+poltrujillo@users.noreply.github.com>
2024-08-13 15:10:14 +0200

    Delete credentials and docs

--- web/src/utils/firebase.js
+++ web/src/utils/firebase.js
@@ -7,13 +7,7 @@ import { getFirestore } from 'firebase/firestore';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBeoT3pTKiMgvCqeQrJojYS516mT5sf-fM',
-  authDomain: 'shuffle-60501.firebaseapp.com',
-  projectId: 'shuffle-60501',
-  storageBucket: 'shuffle-60501.appspot.com',
-  messagingSenderId: '851000526656',
-  appId: '1:851000526656:web:7977305ba7366b81235ca5',
-  measurementId: 'G-8YHBFDMLR1',
+  // Add the firebase credentials
 };
 
 // Initialize Firebase
